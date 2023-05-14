da992b43aac266f5326dc51815c6e22fe8e8161a
AbhinayHinge <117751410+AbhinayHinge@users.noreply.github.com>
2023-05-14 16:26:42 +0530

    changes

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import pickle
+
+import appdirs
 import streamlit as st
 import pandas as pd
 import altair as alt
@@ -32,6 +34,7 @@ medicine['usefulness'] = medicine['rating'] * medicine['usefulCount'] * medicine
 mean_usefulness = medicine['usefulness'].mean()
 
 medicine2 = medicine.drop_duplicates()
+
 
 
 def recommend(condition=list(medicine2['condition'].value_counts().index)):
@@ -128,3 +131,73 @@ if st.button('Show Recommendation'):
                  )
 
     st.altair_chart(top_drugs_chart, use_container_width=True)
+
+from PIL import Image, ImageOps
+if st.button('EDA and Sentiment Analysis of Dataset'):
+    st.title("Sentiment Analysis")
+    image1 = Image.open('sentiment_pie_chart.png')
+    st.image(image1, caption='PIE Chart displays sentiment reviews of users')
+    image2 = Image.open('sentiment2.png')
+    st.image(image2, caption='Correlation Between Sentiment and Rating')
+    image3 = Image.open('Sentiment.png')
+    st.image(image3, caption='Line Chart displays positive, Negative and Neutral reviews aboubt recommended medicines')
+    image4 = Image.open('SEntiment_FeatureIMP.png')
+    st.image(image2, caption='Feature Importance plot using LGBM')
+
+    st.title("Exploratory Data Analysis")
+    image5 = Image.open('eda1.png')
+    st.image(image5, caption='Most popular medicines from dataset')
+    image6 = Image.open('eda2.png')
+    st.image(image6, caption='Most High Rated medicines')
+    image7 = Image.open('eda3.png')
+    st.image(image7, caption='Top 10 and bottom 10 significant medicines')
+    image8 = Image.open('eda4.png')
+    st.image(image8, caption='Top Medicine conditions with highest reviews')
+    image9 = Image.open('eda6.png')
+    st.image(image9, caption='Number of Drugs per condition')
+    image10 = Image.open('eda8.png')
+    st.image(image10, caption='Most popular medicines per useful count')
+    image11 = Image.open('eda5.png')
+    st.image(image11, caption='Average Rating of medicines')
+
+
+import streamlit as st
+from textblob import TextBlob
+
+def analyze_sentiment(text):
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
+    subjectivity = blob.sentiment.subjectivity
+    if polarity > 0:
+        return "positive", polarity, subjectivity
+    elif polarity == 0:
+        return "neutral", polarity, subjectivity
+    else:
+        return "negative", polarity, subjectivity
+
+
+st.title("Add reviews to get Sentiment")
+
+text = st.text_area("Enter some text")
+
+if st.button("Analyze"):
+    words = text.split()
+    top = {}
+    for word in words:
+        if word in top:
+            top[word] += 1
+        else:
+            top[word] = 1
+
+    top_sorted = sorted(top.items(), key=lambda x: x[1], reverse=True)[:50]
+
+    st.markdown("## Top Words")
+    for word, frequency in top_sorted:
+        st.markdown(f"- **{word}**: {frequency}")
+
+    sentiment, polarity, subjectivity = analyze_sentiment(text)
+
+    st.markdown("## Sentiment Analysis")
+    st.markdown(f"- **Sentiment**: {sentiment.capitalize()}")
+    st.markdown(f"- **Polarity**: {polarity}")
+    st.markdown(f"- **Subjectivity**: {subjectivity}")
