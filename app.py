5e4a7deb61538e59a78587b6446c917c357d841a
AbhinayHinge <117751410+AbhinayHinge@users.noreply.github.com>
2023-05-15 11:33:03 +0530

    changes

--- app.py
+++ app.py
@@ -42,8 +42,9 @@ medicine2 = medicine.drop_duplicates()
 
 
 def recommend(condition=list(medicine2['condition'].value_counts().index)):
-    top_drugs = medicine2[medicine2['condition'] == condition][['drugName', 'usefulness']].sort_values(by='usefulness',
-                                                                                                       ascending=False).reset_index(
+    top_drugs = medicine2[medicine2['condition'] == condition][['drugName', 'usefulness']].sort_values(
+        by='usefulness',
+        ascending=False).reset_index(
         drop=True)
     top_drugs = top_drugs.drop_duplicates(subset=['drugName'], keep='first').head(5)
 
@@ -85,36 +86,39 @@ if st.button('Show Recommendation'):
     border_width = 10
     border_color = '#000000'
 
-    with col1:
-        image1 = Image.open(top_drugs2[0])
-        image1_with_border = ImageOps.expand(image1, border=border_width, fill=border_color)
-        st.image(image1_with_border, width=image_width)
-        st.markdown('1. **' + top_drugs.iloc[0]['drugName'] + '**')
+    if len(top_drugs) >= 5:
+        with col1:
+            image1 = Image.open(top_drugs2[0])
+            image1_with_border = ImageOps.expand(image1, border=border_width, fill=border_color)
+            st.image(image1_with_border, width=image_width)
+            st.markdown('1. **' + top_drugs.iloc[0]['drugName'] + '**')
 
-    with col2:
-        image2 = Image.open(top_drugs2[1])
-        image2_with_border = ImageOps.expand(image2, border=border_width, fill=border_color)
-        st.image(image2_with_border, width=image_width)
-        st.markdown('2. **' + top_drugs.iloc[1]['drugName'] + '**')
+        with col2:
+            image2 = Image.open(top_drugs2[1])
+            image2_with_border = ImageOps.expand(image2, border=border_width, fill=border_color)
+            st.image(image2_with_border, width=image_width)
+            st.markdown('2. **' + top_drugs.iloc[1]['drugName'] + '**')
 
-    with col3:
-        image3 = Image.open(top_drugs2[2])
-        image3_with_border = ImageOps.expand(image3, border=border_width, fill=border_color)
-        st.image(image3_with_border, width=image_width)
-        st.markdown('3. **' + top_drugs.iloc[2]['drugName'] + '**')
+        with col3:
+            image3 = Image.open(top_drugs2[2])
+            image3_with_border = ImageOps.expand(image3, border=border_width, fill=border_color)
+            st.image(image3_with_border, width=image_width)
+            st.markdown('3. **' + top_drugs.iloc[2]['drugName'] + '**')
 
-    with col4:
-        image4 = Image.open(top_drugs2[3])
-        image4_with_border = ImageOps.expand(image4, border=border_width, fill=border_color)
-        st.image(image4_with_border, width=image_width)
-        st.markdown('4. **' + top_drugs.iloc[3]['drugName'] + '**')
+        with col4:
+            image4 = Image.open(top_drugs2[3])
+            image4_with_border = ImageOps.expand(image4, border=border_width, fill=border_color)
+            st.image(image4_with_border, width=image_width)
+            st.markdown('4. **' + top_drugs.iloc[3]['drugName'] + '**')
 
-    with col5:
-        image5 = Image.open(top_drugs2[4])
-        image5_with_border = ImageOps.expand(image5, border=border_width, fill=border_color)
-        st.image(image5_with_border, width=image_width)
-        st.markdown('5. **' + top_drugs.iloc[4]['drugName'] + '**')
+        with col5:
+            image5 = Image.open(top_drugs2[4])
+            image5_with_border = ImageOps.expand(image5, border=border_width, fill=border_color)
+            st.image(image5_with_border, width=image_width)
+            st.markdown('5. **' + top_drugs.iloc[4]['drugName'] + '**')
 
+    elif len(top_drugs) < 5:
+        st.write("**Not enough medicines found for the selected condition**")
 
     # Display top 5 drugs and their useful count
     top_drugs_df = top_drugs.reset_index(drop=True)
